b709c22b930e370f62ccf2eff70da6e27b2170b6
九月三秋 <993623938@qq.com>
2019-09-24 16:18:49 +0800

    增加 Input

---
+++ components/BaseInput.vue
@@ -0,0 +1,39 @@
+<template>
+	<view>
+		<input :adjust-position="adjust" :style="{ bottom: bottom+'px' }" class="input" type="text" auto-focus placeholder="请输入" @keyboardheightchange="keyboardheightchange" />
+	</view>
+</template>
+
+<script>
+export default {
+	props: {
+		// 是否上推页面
+		adjust: {
+			type: Boolean,
+			default: true
+		}
+	},
+	data() {
+		return {
+			bottom: 0
+		};
+	},
+	methods: {
+		keyboardheightchange({ detail }) {
+			if (!this.adjust) {
+				this.bottom = detail.height;
+			}
+			if (detail.height === 0) {
+				this.$emit('hide');
+			}
+		}
+	}
+};
+</script>
+
+<style lang="scss">
+.input {
+	position: absolute;
+	bottom: 0;
+}
+</style>
---
+++ pages/input/input.vue
@@ -0,0 +1,27 @@
+<template>
+	<view>
+		<button type="primary" @click="showInput">点我切换输入框</button>
+		<BaseInput v-if="show" @hide="show = false" :adjust="false"></BaseInput>
+	</view>
+</template>
+
+<script>
+import BaseInput from '../../components/BaseInput.vue';
+export default {
+	data() {
+		return {
+			show: false
+		};
+	},
+	methods: {
+		showInput() {
+			this.show = true;
+		}
+	},
+	components: {
+		BaseInput
+	}
+};
+</script>
+
+<style></style>
